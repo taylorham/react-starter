a19cbd13669aa00e29ac1cf0d3eac6d0cb5baaad
Alex DeMars <alexdemars@gmail.com>
2016-09-01 14:51:21 -0700

    adds arrow function to class method declaration, changes comments to block style

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,9 +4,9 @@ import React from 'react';
 // We're using ReactDOM to manage the rendering of our app.
 import ReactDOM from 'react-dom';
 
-// Any components we create must be required at the top of the document.
-// If the component is not the 'default' export from its file, we have to wrap the
-// component name in curly braces for proper import.
+/* Any components we create must be required at the top of the document.
+If the component is not the 'default' export from its file, we have to wrap the
+component name in curly braces for proper import. */
 import { HelloMessage } from './StatelessComponent';
 import Field from './StatefulComponent';
 
@@ -18,8 +18,8 @@ class Main extends React.Component {
     return (
       <div>
         {
-          // Components are included in the page by typing them as though they were
-          // a custom HMTL element and can be passed properties in the same way.
+           /* Components are included in the page by typing them as though they were
+           a custom HMTL element and can be passed properties in the same way. */
         }
         <HelloMessage name="World" />
         <Field />
@@ -28,7 +28,7 @@ class Main extends React.Component {
   }
 }
 
-// The 'Main' class takes care of rendering our entire app. We use ReactDOM to manage this.
-// In the render() function, we must specify the React component we wish to render as well
-// as the html node it will inhabit in our public/index.html file.
+/* The 'Main' class takes care of rendering our entire app. We use ReactDOM to manage this.
+In the render() function, we must specify the React component we wish to render as well
+as the html node it will inhabit in our public/index.html file. */
 ReactDOM.render(<Main />, document.getElementById('app'))
--- app/components/StatefulComponent.js
+++ app/components/StatefulComponent.js
@@ -1,26 +1,28 @@
 import React from 'react';
 
-// All STATEFUL components must be constructed with the React.createClass function.
-// Unlike ES5, we can select the component for export at its definition. If it is the
-// only component we're exporting from the file, we export it with the flag 'default'.
+/* All STATEFUL components must be constructed with the React.createClass function.
+Unlike ES5, we can select the component for export at its definition. If it is the
+only component we're exporting from the file, we export it with the flag 'default'. */
 export default class Field extends React.Component {
-  // In order to track state in this component, we must define a constructor function
-  // which can optionally take an argument of any (props) provided to the component.
-  // This component will have ownership of properties, and will store them in its 'state'.
-  // This state will enable us to display and edit values for the component within our app.
+
+  /* In order to track state in this component, we must define a constructor function
+  which can optionally take an argument of any (props) provided to the component.
+  This component will have ownership of properties, and will store them in its 'state'.
+  This state will enable us to display and edit values for the component within our app. */
   constructor() {
     // Always set super() so that we have access to 'this' in our component for binding.
     super();
     this.state = {
       text: 'Type in the box above!'
     }
-    // Any custom functions defined by the component must be bound in it's constructor for
-    // proper access to it's state with 'this'.
-    this.update = this.update.bind(this);
   }
-  // This is a custom function that we have defined and will apply to our text field
-  // in order to be able to update the state of our component immediately when it changes.
-  update(e) {
+
+  /* This is a custom function that we have defined and will apply to our text field
+  in order to be able to update the state of our component immediately when it changes.
+  Using ES6 arrow syntax, the context of 'this' will always be the container class.
+  In this case, 'this' will always refer to the Field class.  Using this syntax, we can
+  avoid using JavaScript's .bind() method to maintain the correct context. */
+  update = (e) => {
     // setState must be called on 'this' and requires an object for its parameter.
     this.setState({text: e.target.value})
   }
@@ -30,8 +32,8 @@ export default class Field extends React.Component {
       <div>
         <br />
         {
-          // We pass 'this' components 'update' method to the onChange property of our
-          // input in order to bind that function to the DOM element.
+          /* We pass 'this' components 'update' method to the onChange property of our
+          input in order to bind that function to the DOM element. */
         }
         <input className="center-block" type="text" onChange={this.update} />
         {
